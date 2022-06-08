0af006c49430b47a4e072cdee0196e7168daee45
Srajan Rai <srajanzy@gmail.com>
2022-06-08 23:43:27 +0530

    Create 02.c

---
+++ Codes/02.c
@@ -0,0 +1,20 @@
+#include "base.h"
+
+int main() {
+    clear();
+
+    // int a = (4, 6, 5);
+    // printf("%d", a);
+
+    /*
+     int and float gives float
+    */
+
+//    printf("%d", 1534236469< pow(2,31)-1);
+
+    // only True and True True
+    // only False or False False
+    
+
+    return 0;
+}
